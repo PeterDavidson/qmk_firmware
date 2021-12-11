ae0351a7e027d56deb5203fcbb042b55eb06c944
Peter Davidson <peter@transposition.com.au>
2021-12-12 04:30:09 +1000

    Add kyria and minidox layouts

--- keyboards/minidox/config.h
+++ keyboards/minidox/config.h
@@ -40,12 +40,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define BACKLIGHT_LEVELS 3
 
 /* Set 0 if debouncing isn't needed */
-#define DEBOUNCE 5
+#define DEBOUNCE 0
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
+//#define LOCKING_SUPPORT_ENABLE
 /* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
+//#define LOCKING_RESYNC_ENABLE
 
 /* disable debug print */
 //#define NO_DEBUG
